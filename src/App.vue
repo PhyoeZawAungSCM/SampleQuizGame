d6d1f322b2b9de389d4fe87334c729906810e41a
PhyoeZawAungSCM <scm.phyoezawaung@gmail.com>
2023-03-16 08:13:03 +0630

    quiz game

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<!-- <template>
+  <div class="circle" ref="circle">
+  </div>
+  <div class="progress">
+    <div class="progress-background"></div>
+    <div class="progress-status" ref="progress"></div>
+  </div>
+  <button @click="move()">move</button>
+  <input type="number" v-model="percentage"/>
+  <div>
+  <button @click="append">Append</button>
+  <button @click="remove">Remove</button>
+    <ul>
+      <TransitionGroup appear name="list">
+        <li v-for="i in data" :key="i">{{ i }}</li>
+      </TransitionGroup>
+    </ul>
+  </div>
+</template>
+<script setup> 
+  import {gsap} from 'gsap';
+  import {ref,onMounted, computed,watch} from 'vue';
+  const data = ref([1,2,3,4,5,6,7]);
+  const circle = ref(null);
+  const progress = ref(null);
+  const percentage = ref(10);
+  function remove(){
+    data.value.shift()
+  }
+  function append(){
+    data.value.push(data.value.length + 1);
+  }
+  watch(percentage,(p)=>{
+    gsap.to(progress.value,{
+      duration:1,
+      width:p,
+    })
+    console.log(p)
+  })
+  console.log(circle);
+  // onMounted(()=>{
+  //   gsap.to(circle.value, {
+  //   duration: 1.5,
+  //   x: 100,
+  // });
+ // })
+</script>
+<style scoped >
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+  .circle{
+    background-color: rebeccapurple;
+    width:40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+  .progress{
+    width:500px;
+  }
+  .progress-background{
+    background-color: grey;
+    width:100%;
+    height: 5px;
+  }
+  .progress-status{
+    background-color: pink;
+    height: 5px;
+    width:30px;
+  }
+</style> -->
+<script setup>
+ import Loader from './components/Loader.vue'
+</script>
+<template>
+  <Loader/>
+</template>
+<style>
+</style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,65 @@
+<script setup>
+import gsap from 'gsap';
+import { onMounted, ref } from 'vue';
+const isLoading = ref(true);
+const cube1 = ref(null);
+const cube2 = ref(null);
+const cube3 = ref(null);
+const cube4 = ref(null);
+const t1 = gsap.timeline({repeat:-1});
+
+
+onMounted(()=>{
+  t1.fromTo(cube1.value,{x:-50},{x:0,duration:0.3})
+    .fromTo(cube2.value,{x:-10},{duration:0.1,x:10})
+    .fromTo(cube3.value,{x:-10},{duration:0.1,x:10})
+    .fromTo(cube4.value,{x:0},{duration:0.3,x:50})
+    .fromTo(cube4.value,{x:50},{duration:0.3,x:0})
+    .fromTo(cube3.value,{x:10},{duration:0.1,x:-10})
+    .fromTo(cube2.value,{x:10},{duration:0.1,x:-10})
+    .fromTo(cube1.value,{x:10},{x:-50,duration:0.3})
+});
+
+</script> 
+<template>
+  <div class="pageloader">
+    <div class="cube" ref="cube1"></div>
+    <div class="cube" ref="cube2"></div>
+    <div class="cube" ref="cube3"></div>
+    <div class="cube" ref="cube4"></div>
+  </div>
+
+
+</template>
+<style  scoped>
+.pageloader {
+  display: flex;
+  width:100%
+}
+
+.cube {
+  width: 30px;
+  height: 30px;
+  margin: 3px;
+}
+
+.cube:nth-child(1) {
+  background-color: blue;
+  border-radius: 50%;
+}
+
+.cube:nth-child(2) {
+  background-color: brown;
+  border-radius: 50%;
+}
+
+.cube:nth-child(3) {
+  background-color: green;
+  border-radius: 50%;
+}
+
+.cube:nth-child(4) {
+  background-color: black;
+  border-radius: 50%;
+}</style>
+
